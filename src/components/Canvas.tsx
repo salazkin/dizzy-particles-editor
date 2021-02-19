14b91c9550bffe5eb8f22ebb22adf20ae079b9f9
AlexeySalazkin <salazkin@gmail.com>
2021-02-19 20:18:12 +0200

    trigger particles on config update

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,10 +1,27 @@
+import { isEqual } from 'lodash';
 import React, { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 import Stage from '../canvas/Stage';
+let stage = null;
+let currentConfig = null;
 
 const Canvas: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const config = useSelector((state: any) => state.config.output);
 
-    useEffect(() => { new Stage(canvasRef.current!); }, []);
+    useEffect(() => {
+        stage = new Stage(canvasRef.current!);
+    }, []);
+
+
+    useEffect(() => {
+        if (stage && !isEqual(config, currentConfig)) {
+            stage.createParticles(config);
+            currentConfig = config;
+        }
+    });
+
+
     return (
         <canvas ref={canvasRef} onContextMenu={(e) => e.preventDefault()} className="canvas" width={window.innerWidth} height={window.innerHeight}></canvas>
     );
