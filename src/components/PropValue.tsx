c16db6d66f64e2fa6b6a4d29c39c507dbe63f00e
AlexeySalazkin <salazkin@gmail.com>
2021-02-19 20:15:43 +0200

    add escape and enter

--- src/components/PropValue.tsx
+++ src/components/PropValue.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateConfigValue, updateInputValue } from '../actions';
 
+let prvValue = null;
+
 const PropValue: React.FC<{ configKey: string; }> = props => {
 
     const [width, setWidth] = useState(0);
 
-    const strValue = useSelector((state: any) => state.config[props.configKey].strValue);
+    const strValue = useSelector((state: any) => state.config.inputValues[props.configKey]);
 
     const inputRef = useRef<HTMLInputElement>(null);
     const labelRef = useRef<HTMLElement>(null);
@@ -16,7 +18,7 @@ const PropValue: React.FC<{ configKey: string; }> = props => {
     useEffect(() => {
         const rect = labelRef.current!.getBoundingClientRect();
         setWidth(rect.width);
-    });
+    }, [strValue]);
 
     const onChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         dispatch(updateInputValue({ id: props.configKey, value: event.target.value }));
@@ -26,10 +28,33 @@ const PropValue: React.FC<{ configKey: string; }> = props => {
         dispatch(updateConfigValue({ id: props.configKey, value: event.target.value }));
     };
 
+    const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+        if (event.key === "Enter") {
+            inputRef.current.blur();
+        }
+        if (event.key === "Escape") {
+            inputRef.current.value = prvValue;
+            inputRef.current.blur();
+        }
+    };
+
+    const onFocus = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        prvValue = event.target.value;
+    };
+
     return (
         <span className={"input-container"}>
             <span ref={labelRef} className="prop-dummy-value prop-font" >{strValue}</span>
-            <input ref={inputRef} value={strValue} className={"input-value prop-font"} style={{ width: width + 3 }} onChange={onChange} onBlur={onBlur} />
+            <input
+                ref={inputRef}
+                value={strValue}
+                className={"input-value prop-font"}
+                style={{ width: width + 3 }}
+                onChange={onChange}
+                onBlur={onBlur}
+                onKeyDown={onKeyDown}
+                onFocus={onFocus}
+            />
         </span>
     );
 };
