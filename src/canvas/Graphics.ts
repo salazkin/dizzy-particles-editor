54eaa1ccc3fdc11be952df6a5abb85d681d99592
AlexeySalazkin <salazkin@gmail.com>
2021-02-22 16:17:44 +0200

    add drawCirc

--- src/canvas/Graphics.ts
+++ src/canvas/Graphics.ts
@@ -3,20 +3,39 @@ export default class Graphics {
 
 
     public static drawRect(id: string, width: number, height: number, color: string): Promise<HTMLImageElement> {
-        let canvas = document.createElement('canvas');
+        const canvas = document.createElement('canvas');
         canvas.width = width;
         canvas.height = height;
 
-        let context = canvas.getContext('2d')!;
+        const context = canvas.getContext('2d')!;
         context.fillStyle = color;
         context.fillRect(0, 0, width, height);
 
-        let img = new Image();
+        const img = new Image();
         img.id = id;
         img.src = canvas.toDataURL("image/png");
 
         return new Promise(resolve => {
-            img.addEventListener("load", () => { resolve(img) });
+            img.addEventListener("load", () => { resolve(img); });
+        });
+    }
+
+    public static drawCirc(id: string, radius: number, color: string): Promise<HTMLImageElement> {
+        const canvas = document.createElement('canvas');
+        canvas.width = radius;
+        canvas.height = radius;
+        const circle = new Path2D();
+        circle.arc(radius * 0.5, radius * 0.5, radius * 0.5, 0, 2 * Math.PI, false);
+        const context = canvas.getContext('2d')!;
+        context.fillStyle = color;
+        context.fill(circle);
+
+        const img = new Image();
+        img.id = id;
+        img.src = canvas.toDataURL("image/png");
+
+        return new Promise(resolve => {
+            img.addEventListener("load", () => { resolve(img); });
         });
     }
 
