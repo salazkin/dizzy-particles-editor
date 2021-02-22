4379fee5114d86eab32ba360ab136546f3592fb4
AlexeySalazkin <salazkin@gmail.com>
2021-02-22 18:34:50 +0200

    add types, fix scale

--- src/particles/Particles.ts
+++ src/particles/Particles.ts
@@ -194,7 +194,7 @@ export default class Particles extends BaseParticles<ParticleData> {
 
 
         particle.alpha = config.alphaFrom + (config.alphaTo - config.alphaFrom) * (config.alphaYoYo ? yoyoTime : t);
-        particle.scale = Utils.interpolate(config.scaleYoYo ? yoyoTime : t, config.scaleFrom, config.scaleTo);
+        particle.scaleX = particle.scaleY = Utils.interpolate(config.scaleYoYo ? yoyoTime : t, config.scaleFrom, config.scaleTo);
 
 
         if (config.curve) {
@@ -240,18 +240,15 @@ export default class Particles extends BaseParticles<ParticleData> {
     }
 }
 
-
-
-
 const Utils = {
-    getRotationOnCurve(t, p1, p2, cp1, cp2) {
+    getRotationOnCurve(t: number, p1: Point, p2: Point, cp1: Point, cp2: Point): number {
         const t2 = t * t;
         const dx = 3 * Math.pow(1 - t, 2) * (cp1.x - p1.x) + 6 * (1 - t) * t * (cp2.x - cp1.x) + 3 * t2 * (p1.y - cp2.x);
         const dy = 3 * Math.pow(1 - t, 2) * (cp1.y - p1.y) + 6 * (1 - t) * t * (cp2.y - cp1.y) + 3 * t2 * (p2.y - cp2.y);
         return Math.atan2(dx, dy);
     },
 
-    hexToRgb(color) {
+    hexToRgb(color: string): number[] {
         const arr: number[] = [];
         for (let i = 2; i >= 0; i--) {
             const c = color.substring(color.length - i * 2 - 2, color.length - i * 2);
@@ -260,11 +257,11 @@ const Utils = {
         return arr;
     },
 
-    rgbToHex(arr, prefix) {
+    rgbToHex(arr: number[], prefix: string): string {
         return prefix + arr.map(v => this.hexValue(v)).join("");
     },
-    //export_skip
-    getInterpolatedColors(hexArr, steps): string[] {
+
+    getInterpolatedColors(hexArr: string[], steps: number): string[] {
         const arr = hexArr.map(hex => this.hexToHsl(hex));
         const out: string[] = [];
         const step = 1 / steps;
@@ -279,7 +276,7 @@ const Utils = {
         return out;
     },
 
-    interpolate(t, v1, v2, minDist?) {
+    interpolate(t: number, v1: number, v2: number, minDist?: number): number {
         if (!minDist) {
             return v1 + (v2 - v1) * t;
         }
@@ -294,7 +291,7 @@ const Utils = {
         }
     },
 
-    hexToHsl(hex) {
+    hexToHsl(hex: string): number[] {
         const r = parseInt(hex.substring(hex.length - 6, hex.length - 4), 16) / 255;
         const g = parseInt(hex.substring(hex.length - 4, hex.length - 2), 16) / 255;
         const b = parseInt(hex.substring(hex.length - 2, hex.length), 16) / 255;
@@ -318,7 +315,7 @@ const Utils = {
         return [h, s, l]; //[0,1] range
     },
 
-    hslToHex(arr): string {
+    hslToHex(arr: number[]): string {
         let h = arr[0];
         let s = arr[1];
         let l = arr[2];
@@ -336,7 +333,7 @@ const Utils = {
         return "0x" + this.hexValue(r) + this.hexValue(g) + this.hexValue(b);
     },
 
-    hueToRgb(t, p, q) {
+    hueToRgb(t: number, p: number, q: number): number {
         if (t < 0) {
             t += 1;
         }
@@ -355,24 +352,24 @@ const Utils = {
         return p;
     },
 
-    hexValue(v) {
+    hexValue(v: number): string {
         return ("0" + v.toString(16)).slice(-2);
     },
 
-    degreeToRadians(degrees) {
+    degreeToRadians(degrees: number): number {
         return degrees * Math.PI / 180;
     },
 
-    radiansToDegree(radians) {
+    radiansToDegree(radians: number): number {
         return radians * 180 / Math.PI;
     },
 
-    setPositionOnLine(target, t, p0, p1) {
+    setPositionOnLine(target: Point, t: number, p0: Point, p1: Point): void {
         target.x = p0.x + (p1.x - p0.x) * t;
         target.y = p0.y + (p1.y - p0.y) * t;
     },
 
-    setPositionOnCurve(target, t, p1, p2, cp1, cp2) {
+    setPositionOnCurve(target: Point, t: number, p1: Point, p2: Point, cp1: Point, cp2: Point): void {
         const t2 = t * t;
         const t3 = t * t * t;
         target.x = Math.pow(1 - t, 3) * p1.x + 3 * Math.pow(1 - t, 2) * t * cp1.x + 3 * (1 - t) * t2 * cp2.x + t3 * p2.x;
